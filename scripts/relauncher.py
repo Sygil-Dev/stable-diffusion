3402c1a459479a0770d37ef0d29a7e35c2e566b7
David <DarkVamprism@users.noreply.github.com>
2022-09-03 16:46:24 +0800

    Update relauncher.py to accept command line

--- scripts/relauncher.py
+++ scripts/relauncher.py
@@ -1,11 +1,13 @@
-import os, time
+import os, time, sys
+
+argString = ' '.join(sys.argv[1:])
 
 n = 0
 while True:
     print('Relauncher: Launching...')
     if n > 0:
         print(f'\tRelaunch count: {n}')
-    os.system("python scripts/webui.py")
+    os.system("python scripts/webui.py " + argString)
     print('Relauncher: Process is ending. Relaunching in 0.5s...')
     n += 1
     time.sleep(0.5)
